01a077a089e213a155f348acf1ecb459134beb49
thorning <brian@thornings.dk>
2019-02-18 10:19:38 +0100

    Add project files.

---
+++ ClientMadbordet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientMadbordet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
